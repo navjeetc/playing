166338b584ce1ecee1a06512295c3c3dc035e861
Navjeet Chabbewal <navjeetc@gmail.com>
2014-07-22 03:03:45 +0100

    fixed timezone related bug

--- app/models/commitment.rb
+++ app/models/commitment.rb
@@ -1,9 +1,11 @@
 class Commitment < ActiveRecord::Base
   belongs_to :user
 
-  scope :for_today, -> { where(start_at: (Date.today..(Date.today+1.day))).order(:start_at) }
-  scope :for_tomorrow, -> { where(start_at: ((Date.today+1.day)..(Date.today + 2.day))).order(:start_at) }
-  scope :for_future, -> {where("start_at >= ?", Date.today).order(:start_at)}
+  NOW = DateTime.now.in_time_zone
+  TOMORROW = (NOW+1.day).in_time_zone
+  scope :for_today, -> { where(start_at: (NOW.beginning_of_day..NOW.end_of_day)).order(:start_at) }
+  scope :for_tomorrow, -> { where(start_at: (NOW.end_of_day..TOMORROW.end_of_day)).order(:start_at) }
+  scope :for_future, -> {where("start_at >= ?", NOW.beginning_of_day).order(:start_at)}
 
   def self.for_date(date)
   	(date == "today") ? Commitment.for_today : ((date == "tomorrow") ? Commitment.for_tomorrow : Commitment.for_future)
