2340bcf255e111eb3390b7b8dd9f2cc8841ea214
Navjeet Chabbewal <navjeetc@gmail.com>
2014-07-30 02:12:56 +0100

    fixed time display isssue UTC vs EST

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
 	def today
-		content_tag(:div, content_tag(:span, "Today is #{Date.today.strftime("%A %B %d, %Y")}"), class: "date_today")
+		content_tag(:div, content_tag(:span, "Today is #{Time.zone.now.strftime("%A %B %d, %Y")}"), class: "date_today")
 	end
 end
--- app/models/commitment.rb
+++ app/models/commitment.rb
@@ -5,7 +5,7 @@ class Commitment < ActiveRecord::Base
   TOMORROW = (NOW+1.day).in_time_zone
   scope :for_today, -> { where(start_at: (NOW.beginning_of_day..NOW.end_of_day)).order(:start_at) }
   scope :for_tomorrow, -> { where(start_at: (NOW.end_of_day..TOMORROW.end_of_day)).order(:start_at) }
-  scope :for_future, -> {where("start_at >= ?", NOW.beginning_of_day).order(:start_at)}
+  scope :for_future, -> {where("start_at >= ?", TOMORROW.beginning_of_day).order(:start_at)}
 
   def self.for_date(date)
   	(date == "today") ? Commitment.for_today : ((date == "tomorrow") ? Commitment.for_tomorrow : Commitment.for_future)
